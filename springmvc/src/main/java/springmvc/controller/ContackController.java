5616dde61fee144913d7620ae033f120ea97660e
akash <akash@LAPTOP-8F3BRJOB>
2022-01-10 09:49:16 +0530

    updated

--- springmvc/src/main/java/springmvc/controller/ContackController.java
+++ springmvc/src/main/java/springmvc/controller/ContackController.java
@@ -39,27 +39,24 @@ public class ContackController {
 		return "newpage";
 	}
 
-}
+//	@RequestMapping(path = "/processform", method = RequestMethod.POST)
+//	public String printConctact(@RequestParam("emial") String userMail, @RequestParam("userName") String usrName,
+//			@RequestParam("userPassword") String usrPassword, Model model) {
+//
+//		System.out.println("email " + userMail);
+//		System.out.println("name " + usrName);
+//		System.out.println("password " + usrPassword);
+//
+//		User user = new User(userMail, usrName, usrPassword);
+//		System.out.println(user);
+//
+//		model.addAttribute("mail", userMail);
+//		model.addAttribute("name", usrName);
+//		model.addAttribute("pswd", usrPassword);
+//
+//		model.addAttribute("user", user);
+//
+//		return "newpage";
+//	}
 
-/*
- * @RequestMapping(path = "/processform", method = RequestMethod.POST) public
- * String printConctact(@RequestParam("emial") String
- * userMail, @RequestParam("userName") String usrName,
- * 
- * @RequestParam("userPassword") String usrPassword, Model model) {
- * 
- * System.out.println("email " + userMail); System.out.println("name " +
- * usrName); System.out.println("password " + usrPassword);
- * 
- * User user = new User(userMail, usrName, usrPassword);
- * System.out.println(user);
- * 
- * // model.addAttribute("mail", userMail); // model.addAttribute("name",
- * usrName); // model.addAttribute("pswd", usrPassword);
- * 
- * model.addAttribute("user", user);
- * 
- * return "newpage"; }
- * 
- * }
- */
+}
